25fe6606fc38c7954d00f01c7d19a5c2c4622758
Qasatka <bekzhan.aitenov@gmail.com>
2023-03-15 23:56:34 +0200

    final

--- test.py
+++ test.py
@@ -10,30 +10,33 @@ def cut_song(song):
   
   song_pieces = []
 
-  while start + 10000 < end:
-    song_pieces.append(song[start:start+10000])
+  while start + 20000 < end:
+    song_pieces.append(song[start:start+20000])
 
-    start += 10000
+    start += 20000
   
   return song_pieces
+
 def prepare_song(song_path):
   list_matrices = []
-  y,sr = librosa.load(song_path,sr=22050)
+  y,sr = librosa.load(song_path,sr=41000)
+  
   song_pieces = cut_song(y)
   for song_piece in song_pieces:
     melspect = librosa.feature.melspectrogram(song_piece)
     list_matrices.append(melspect)
   return list_matrices
-fs = 44100  # Sample rate
+
+fs = 41000  # Sample rate
 seconds = 3  # Duration of recording
 print("speak")
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
 write('output.wav', fs, myrecording)  # Save as WAV file
 song_path = 'output.wav'
-x,sr = librosa.load(song_path,sr=22050)
+x,sr = librosa.load(song_path,sr=41000)
 x = prepare_song(song_path)
 x=np.expand_dims(x, axis=-1)
-new_model = tf.keras.models.load_model('saved_model/first_model')
+new_model = tf.keras.models.load_model('/home/bekzhan/Desktop/dsd/DSD-Project/saved_model/first_model')
 ypred = new_model.predict(x)
 print(ypred)
